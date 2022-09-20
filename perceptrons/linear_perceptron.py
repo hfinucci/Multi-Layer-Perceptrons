9939e3d0f42c8b2567a3d9f72d75e4d56facb48c
Azul <azkim@itba.edu.ar>
2022-09-20 16:53:16 -0300

    Feat: Added Linear Perceptron

---
+++ perceptrons/linear_perceptron.py
@@ -0,0 +1,17 @@
+from perceptrons.Perceptron import Perceptron
+import numpy as np
+
+class LinearPerceptron(Perceptron):
+
+    def __init__(self, training, expected_output, learning_rate):
+        super().__init__(training, expected_output, learning_rate)
+
+    def activation(self, excitation):
+        return excitation
+
+    def error(self, w):
+        error = 0
+        for i in range(len(self.training)):
+            output = np.inner(self.training[i], w)
+            error += (self.expected_output[i] - output) ** 2
+        return error / 2
