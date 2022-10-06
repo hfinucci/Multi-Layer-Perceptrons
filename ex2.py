5365c5eb09b5b307e4cd63c88d8cf2f704ba1e45
Azul <azkim@itba.edu.ar>
2022-10-06 18:20:22 -0300

    Fix: Updated main for exercise 2 (linear and non-linear)
    Co-authored-by: Juan <jdeluca@itba.edu.ar>

--- ex2.py
+++ ex2.py
@@ -3,6 +3,9 @@ import matplotlib
 matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
 import csv
+import json
+from perceptrons.linear_perceptron import LinearPerceptron
+from perceptrons.non_linear_perceptron import NonLinearPerceptron
 
 x = []
 y = []
@@ -11,12 +14,15 @@ z = []
 
 with open('Ejer2/TP2-ej2-conjunto.csv', 'r') as csvfile:
     plots = csv.reader(csvfile, delimiter=',')
+    data = []
+    expected_output = []
 
     for row in plots:
         x.append(float(row[0]))
         y.append(float(row[2]))
         z.append(float(row[3]))
-
+        data.append([float(row[0]), float(row[1]), float(row[2])])
+        expected_output.append(float(row[3]))
 
     #pnt promedio
     x1 = sum(x)/len(x)
@@ -47,5 +53,17 @@ with open('Ejer2/ex2_config.json') as file:
     jsonObject = json.load(file)
     file.close()
 
+perceptron = str(jsonObject["perceptron"])
 learning_rate = float(jsonObject["learning_rate"])
 generation = int(jsonObject["generation"])
+
+if (perceptron == "linear"):
+    perceptron = LinearPerceptron(data, expected_output, learning_rate)
+    errors, min_w = perceptron.train(generation)
+    print("Errors: " + str(errors))
+    print("Min w: " + str(min_w))
+else:
+    perceptron = NonLinearPerceptron(data, expected_output, learning_rate)
+    errors, min_w = perceptron.train(generation)
+    print("Errors: " + errors)
+    print("Min w: " + min_w)
